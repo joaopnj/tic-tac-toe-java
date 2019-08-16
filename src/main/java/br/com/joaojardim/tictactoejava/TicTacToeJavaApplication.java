02fd8625b15d514e1d7a8f5d72200548499cc183
João Jardim <joaopnj19@gmail.com>
2019-08-15 21:30:12 -0300

    Init project

---
+++ src/main/java/br/com/joaojardim/tictactoejava/TicTacToeJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaojardim.tictactoejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaojardim/tictactoejava/TicTacToeJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.joaojardim.tictactoejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicTacToeJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
